0621264740c0fafa7c649f3232ce9b58371c47e3
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-20 23:04:16 -0400

    maybe

--- configs/cors.js
+++ configs/cors.js
@@ -1,7 +1,7 @@
 // The White List Array are urls allowed to make API requests in production mode
 // IN dev mode "npm run dev" any source can make a request
 var whitelist = ["http://example1.com", "http://example2.com"];
-var corsOptions = {
+let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
