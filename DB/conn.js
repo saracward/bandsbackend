0567bdc0fe8c23e81c5df67a1e04335ef1beae82
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-20 22:56:52 -0400

    a few edits on the server

--- DB/conn.js
+++ DB/conn.js
@@ -1,19 +1,12 @@
-///////////////////////////
-// Environmental Variables
-///////////////////////////
 require("dotenv").config();
-
-/////////////////////////////////////
-// MONGOOSE CONNECTION
-/////////////////////////////////////
 const { MONGODBURI } = process.env;
 const mongoose = require("mongoose");
 const config = { useUnifiedTopology: true, useNewUrlParser: true };
-const DB = mongoose.connection;
+const db = mongoose.connection;
 
 mongoose.connect(MONGODBURI, config);
 
-DB.on("open", () => console.log("You are connected to Mongo"))
+db.on("open", () => console.log("You are connected to Mongo"))
   .on("close", () => console.log("You are disconnected to Mongo"))
   .on("error", (err) => console.log(err));
 
